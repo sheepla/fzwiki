66a7992084fe8472857ceea4ce4ccc85d99e5661
Sheepla <62412884+sheepla@users.noreply.github.com>
2022-01-05 16:18:26 +0900

    [update] language can be set with env var

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 const (
 	appVersion = "0.0.4"
 	appName    = "fzwiki"
+    envNameLang = "FZWIKI_LANG"
 )
 
 type options struct {
@@ -69,7 +70,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	result := searchArticles(strings.Join(args, " "), opts.Language)
+    var lang string
+    if opts.Language == "" {
+        lang = os.Getenv(envNameLang)
+    } else {
+        lang = opts.Language
+    }
+
+    result := searchArticles(strings.Join(args, " "), lang)
+
 	for i := 0; i < len(result.Query.Search); i++ {
 		if t, err := html2text(result.Query.Search[i].Title); err == nil {
 			result.Query.Search[i].Title = t
